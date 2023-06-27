1049598f5241dfda3a3f6a5c2d4db89fc12159ae
pk <pablo.kogan@fi.uncoma.edu.ar>
2023-06-27 08:29:18 -0300

    formosa cordoba y guatemala

---
+++ salidagoblariojajujuymisiones2023/scraper.php
@@ -0,0 +1,85 @@
+<?php
+//$urloficial='https://neuquen.datosoficiales.com/resultados/';
+$urloficial='https://resultados.tsje.gov.py/publicacion/dinamics/divulgacion.ajax.php?codeleccion=37&candidatura=1';
+$str = file_get_contents($urloficial);
+$resultados = json_decode($str, true); 
+$jsonmesas=json_encode($resultados,JSON_PRETTY_PRINT);
+$cont_mesas='XX';
+$carpeta=dirname(__FILE__); 
+//echo $carpeta; exit;
+file_put_contents($carpeta.'/salida/'.date('ymd His ').'mesas '.$cont_mesas.'.json', $jsonmesas);
+ 
+$hora=$resultados['horaFormated'];
+$candidatos=$resultados['candidatos'];
+$linea=$resultados['totales'];
+foreach ($candidatos as $key => $value) {
+    $linea[]=$value['votos'];
+}
+$linea[]=$hora;
+$fecha_actual = date("d-m-Y H:i:s");
+$linea[]=$fecha_actual;
+$fp = fopen($carpeta.'/salida/log.csv', 'r+');
+fseek($fp, 0, SEEK_END);
+fputcsv($fp, $linea);
+/*//http://www.datosoficiales.com/
+//$str = file_get_contents('API/cargos.json');
+//Now decode the JSON using json_decode():
+
+//$cargos = json_decode($str, true); // decode the JSON into an associative array
+
+
+//echo '<pre>' . print_r($cargos, true) . '</pre>';
+
+$str = file_get_contents('API/ubicaciones.json');
+//Now decode the JSON using json_decode():
+
+$ubicaciones = json_decode($str, true); // decode the JSON into an associative array
+
+//print_r();
+//echo '<pre>' . print_r($ubicaciones, true) . '</pre>';
+$mesas=[];
+//se arma un array con todas las urls
+
+foreach ($ubicaciones as $key => $value) {
+    if($value['clase_ubicacion']=='Mesa'){
+        //print_r($value['ancestros']);
+        
+        $url=$urloficial. str_replace('.', '/', $value['id_ubicacion']).'/GOB.json';//'/RPO.json';//'/CNJ.json';
+        $mesas[$value['descripcion_ubicacion']]['numero']=$value['descripcion_ubicacion'];
+        $mesas[$value['descripcion_ubicacion']]['url']=$url;
+    //    echo $url.'<br>';
+        
+    }
+}
+//posta https://neuquen.datosoficiales.com/resultados/NE/1/22/52/59/GOB.json
+////https://neuquen.datosoficiales.com/#resultados/NE/1/22/95/616GOB.json
+//echo '<pre>' . print_r($mesas, true) . '</pre>';
+//se descargan todas las mesas del sitio oficial y se pasan a un array mesas2 con los resultados
+$cont_mesas=0;
+$mesas2=[];
+foreach ($mesas as $key1 => $value1) {
+        echo $value1['url'];
+        if($str = file_get_contents($value1['url']) ){
+            $cont_mesas++;
+            $mesa = json_decode($str, true);
+            echo "Si esta \n";
+            //echo '<pre>' . print_r($mesa , true) . '</pre>';
+            $mesa2=$value1;
+            $mesa2['hora_proceso']=$mesa['hora_proceso'];
+            foreach ($mesa['resultados'] as $key => $value) {
+              $mesa2[$value['id_candidatura']]= $value['cant_votos'];    
+            }
+            $mesas2[]=$mesa2;
+            
+    }else{
+        echo "No esta\n";
+    }
+}
+
+//echo '<pre>' . print_r($mesas, true) . '</pre>';
+
+//se escribe lo escrapeado en un json
+$jsonmesas=json_encode($mesas2,JSON_PRETTY_PRINT);
+file_put_contents('salida/'.date('ymd His ').'mesas '.$cont_mesas.'.json', $jsonmesas);
+
+*/
